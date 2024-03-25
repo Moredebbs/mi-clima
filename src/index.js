090e8fae15002aca3133c24d462bef0d98620bf2
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2024-03-25 23:43:19 +0000

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function refreshWeather(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
@@ -7,16 +6,38 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
-  let date = newDate(response.data.time * 1000);
+  console.log(timeElement);
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = date.getHours();
+
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "80428f51a21b4t3dfe9d3b547of6cb3f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
@@ -30,6 +51,6 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
-
+console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("London");
